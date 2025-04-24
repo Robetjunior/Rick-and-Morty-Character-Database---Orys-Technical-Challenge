9ea1736373811450220b0d653d417e187a011305
Robetjunior <juniorferreirarob@gmail.com>
2025-04-24 00:41:24 -0300

    feat: add character context - src/contexts/CharacterContext.tsx

--- src/contexts/CharacterContext.tsx
+++ src/contexts/CharacterContext.tsx
@@ -1,3 +1,4 @@
+
 import { createContext, useContext, ReactNode, useMemo } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { fetchCharacters } from "@/services/character";
@@ -8,9 +9,13 @@ type CharacterContextType = {
   characters: Character[];
   isLoading: boolean;
   isError: boolean;
+  isPending: boolean;
+  isFetching: boolean;
   isFetchingNextPage: boolean;
   hasNextPage: boolean;
   fetchNextPage: () => void;
+  error: Error | null;
+  refetch: () => Promise<any>;
 };
 
 const CharacterContext = createContext<CharacterContextType | undefined>(undefined);
@@ -39,6 +44,10 @@ export const CharacterProvider = ({
     isLoading,
     isError,
     isFetchingNextPage,
+    isPending,
+    isFetching,
+    error,
+    refetch,
   } = useInfiniteQuery({
     queryKey: ["characters", searchTerm],
     queryFn: ({ pageParam = 1 }) => fetchCharacters(pageParam, searchTerm),
@@ -47,6 +56,9 @@ export const CharacterProvider = ({
         ? parseInt(new URL(lastPage.info.next).searchParams.get("page") || "1")
         : undefined,
     initialPageParam: 1,
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    gcTime: 1000 * 60 * 10, // 10 minutes
+    refetchOnWindowFocus: false,
     meta: {
       onError: () => {
         toast({
@@ -67,9 +79,13 @@ export const CharacterProvider = ({
     characters,
     isLoading,
     isError,
+    isPending,
+    isFetching,
     isFetchingNextPage,
     hasNextPage,
     fetchNextPage,
+    error,
+    refetch,
   };
 
   return (
